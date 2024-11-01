150553af30c79806459756bac7e548df5e9aa1dc
S <s>
2024-11-01 23:48:28 +0400

    {"types":"C","runs":48}

--- Breakout.java
+++ Breakout.java
@@ -68,10 +68,10 @@ public class Breakout extends GraphicsProgram {
 			for(int i = BRICK_SEP/2; i < APPLICATION_WIDTH; i += BRICK_WIDTH + BRICK_SEP) {
 				GRect rect = new GRect(i,j,BRICK_WIDTH,BRICK_HEIGHT);
 				rect.setFilled(true);
-				add(rect);
 				rect.setColor(Color.getColor(str[paint]));
 				rect.setFillColor(Color.getColor(str[paint]));
 				
+				add(rect);
 			}
 		}
 		
