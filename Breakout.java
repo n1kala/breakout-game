c53dd3751ca9c90b04da349d225e6ed76b126963
S <s>
2024-11-02 03:49:54 +0400

    {"types":"C","runs":123}

--- Breakout.java
+++ Breakout.java
@@ -130,12 +130,13 @@ public class Breakout extends GraphicsProgram {
 		ball.setFilled(true);
 		add(ball);
 		double arr [] = {Math.random()*10 + 0.1, Math.random()*5 + 0.1};
+		print(arr[0] + " " + arr[1]);
 		while(true) {
 			ball.setLocation(ball.getX() + arr[0], ball.getY() + arr[1]);
 			double padleX = getMousePosition().x - padle.getX();
 			if(padleX > 0) padleX = 1; 
 			else padleX = -1;
-			padle.setLocation(padle.getX() + padleX + 50, padle.getY());
+			padle.setLocation(padle.getX() + padleX, padle.getY());
 			try {
 			    Thread.sleep(10); // Pause for 1 second
 			} catch (InterruptedException e) {
