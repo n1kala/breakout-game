604be6585e5847ff201e70529a977af56b5d0520
S <s>
2024-11-02 00:17:48 +0400

    {"types":"C","runs":76}

--- Breakout.java
+++ Breakout.java
@@ -64,16 +64,16 @@ public class Breakout extends GraphicsProgram {
 		setSize(APPLICATION_WIDTH + 18, APPLICATION_HEIGHT + 72);
 		print(getHeight());
 		float [][] colors = {
-				{0.0f,0.9f,0.9f},
-				{0.0f,0.9f,0.9f},
-				{0.1f,0.9f,0.9f},
-				{0.1f,0.9f,0.9f},
-				{0.15f,0.9f,0.9f},
-				{0.15f,0.9f,0.9f},
-				{0.3f,0.9f,0.9f},
-				{0.3f,0.9f,0.9f},
-				{0.5f,0.9f,0.9f},
-				{0.5f,0.9f,0.9f}
+				{0.0f,0.99f,0.99f},
+				{0.0f,0.99f,0.99f},
+				{0.1f,0.99f,0.99f},
+				{0.1f,0.99f,0.99f},
+				{0.15f,0.99f,0.99f},
+				{0.15f,0.99f,0.99f},
+				{0.3f,0.99f,0.99f},
+				{0.3f,0.99f,0.99f},
+				{0.5f,0.99f,0.99f},
+				{0.5f,0.99f,0.99f}
 		};
 		for(int j = 70,paint = 0; j < BRICK_Y_OFFSET+NBRICK_ROWS*BRICK_HEIGHT + (NBRICK_ROWS-1)*BRICK_SEP; j += BRICK_HEIGHT + BRICK_SEP, paint++) {
 			for(int i = BRICK_SEP/2; i < APPLICATION_WIDTH; i += BRICK_WIDTH + BRICK_SEP) {
