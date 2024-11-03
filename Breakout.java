a2538d8f6395ca8af8caaa2b75aa91daf36f8406
S <s>
2024-11-03 16:01:36 +0400

    {"types":"C","runs":8}

--- Breakout.java
+++ Breakout.java
@@ -219,8 +219,8 @@ public class Breakout extends GraphicsProgram {
 				brickIsLeft = true;
 				// when ball hits block from left or from right
 				if((ball.getY() + BALL_RADIUS <= bricks[i][j].getY() + BRICK_HEIGHT && ball.getY() + BALL_RADIUS >= bricks[i][j].getY()) &&
-						((ball.getX() + BALL_RADIUS*2 >= bricks[i][j].getX() && ball.getX() + BALL_RADIUS*2 <= bricks[i][j].getX() + ballMovementDirections[0] + 2) ||
-						(ball.getX() <= bricks[i][j].getX() + BRICK_WIDTH && ball.getX() >= bricks[i][j].getX() + BRICK_WIDTH + ballMovementDirections[0] - 2))) {
+						((ball.getX() + BALL_RADIUS*2 >= bricks[i][j].getX() && ball.getX() + BALL_RADIUS*2 <= bricks[i][j].getX() + ballMovementDirections[0]) ||
+						(ball.getX() <= bricks[i][j].getX() + BRICK_WIDTH && ball.getX() >= bricks[i][j].getX() + BRICK_WIDTH + ballMovementDirections[0]))) {
 					bricks[i][j].setFilled(false);
 					remove(bricks[i][j]);
 					ballMovementDirections[0] *= -1;
@@ -228,8 +228,8 @@ public class Breakout extends GraphicsProgram {
 				} else {
 					// when ball hits block from top or from bottom
 					if(ball.getX() + BALL_RADIUS*2 >= bricks[i][j].getX() && ball.getX() <= bricks[i][j].getX() + BRICK_WIDTH &&
-							((ball.getY() >= bricks[i][j].getY() + BRICK_HEIGHT + ballMovementDirections[1] - 2 && ball.getY() <= bricks[i][j].getY() + BRICK_HEIGHT) ||
-							(ball.getY() + BALL_RADIUS*2 >= bricks[i][j].getY() && ball.getY() + BALL_RADIUS*2 <= bricks[i][j].getY()+ballMovementDirections[1]+2))) {
+							((ball.getY() >= bricks[i][j].getY() + BRICK_HEIGHT + ballMovementDirections[1] && ball.getY() <= bricks[i][j].getY() + BRICK_HEIGHT) ||
+							(ball.getY() + BALL_RADIUS*2 >= bricks[i][j].getY() && ball.getY() + BALL_RADIUS*2 <= bricks[i][j].getY()+ballMovementDirections[1]))) {
 						bricks[i][j].setFilled(false);
 						remove(bricks[i][j]);
 						ballMovementDirections[1] = -ballMovementDirections[1];
