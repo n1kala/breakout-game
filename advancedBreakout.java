3e642de2ae6047d1610830ef29068c497910b748
S <s>
2024-11-03 15:23:19 +0400

    {"types":"A","runs":0}

---
+++ advancedBreakout.java
@@ -0,0 +1,323 @@
+/*
+ * File: Breakout.java
+ * -------------------
+ * Name:
+ * Section Leader:
+ * 
+ * This file will eventually implement the game of Breakout.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import sun.java2d.loops.DrawPolygons;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class advancedBreakout extends GraphicsProgram {
+
+/** Width and height of application window in pixels */
+	public static final int APPLICATION_WIDTH = 400;
+	public static final int APPLICATION_HEIGHT = 600;
+
+/** Dimensions of game board (usually the same) */
+	private static final int WIDTH = APPLICATION_WIDTH;
+	private static final int HEIGHT = APPLICATION_HEIGHT;
+
+/** Dimensions of the paddle */
+	private static final int PADDLE_WIDTH = 60;
+	private static final int PADDLE_HEIGHT = 10;
+
+/** Offset of the paddle up from the bottom */
+	private static final int PADDLE_Y_OFFSET = 30;
+
+/** Number of bricks per row */
+	private static final int NBRICKS_PER_ROW = 10;
+
+/** Number of rows of bricks */
+	private static final int NBRICK_ROWS = 10;
+
+/** Separation between bricks */
+	private static final int BRICK_SEP = 4;
+
+/** Width of a brick */
+	private static final int BRICK_WIDTH =
+	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+
+/** Height of a brick */
+	private static final int BRICK_HEIGHT = 8;
+
+/** Radius of the ball in pixels */
+	private static final double BALL_RADIUS = 5;
+
+/** Offset of the top brick row from the top */
+	private static final int BRICK_Y_OFFSET = 70;
+
+/** Number of turns */
+	private static final int NTURNS = 3;
+
+/* Method: run() */
+/** Runs the Breakout program. */
+	public void run() {
+		/* for some reason setSize does not set size same as passed values so I needed to add 18 and 72*/
+		setSize(APPLICATION_WIDTH + 18, APPLICATION_HEIGHT + 72);
+		setPlayButton();
+		GRect [][] bricks = setBricks();
+		GRect paddle = setPaddle();
+		GOval ball = setBall();
+		setFrame();
+		startGame(bricks, paddle, ball);
+	}
+	
+	// function sets up play button and waits until player clicks mouse to start the game
+	private void setPlayButton() {
+		int R = 40;
+		int x = WIDTH/2 - R;
+		int y = HEIGHT/2 - R;
+		
+		GOval playButton = new GOval(x,y,R*2,R*2);
+		playButton.setFilled(true);
+		playButton.setColor(Color.GREEN);
+		add(playButton);
+		
+		decoratePlayButton();
+		
+		waitForClick();
+		removeAll();
+	}
+	
+	// i tried to add triangle to make button look like play button
+	// but its just too many white lines bc i dont know how to make triangle
+	private void decoratePlayButton() {
+		int x = WIDTH/2 - 18;
+		int y = HEIGHT/2 - 30;
+		int x1 = x + 50;
+		int y1 = y + 30;
+		while(y != y1+30) {
+			GLine line = new GLine(x,y,x1,y1);
+			line.setColor(Color.WHITE);
+			add(line);
+			y++;
+		}
+	}
+	
+	// function sets up bricks according to color requirements and returns bricks array
+	private GRect [][] setBricks() {
+		GRect [][] bricks = new GRect[NBRICK_ROWS][NBRICKS_PER_ROW];
+		
+		float [][] colors = {
+				{0.0f,0.99f,0.99f},
+				{0.0f,0.99f,0.99f},
+				{0.1f,0.99f,0.99f},
+				{0.1f,0.99f,0.99f},
+				{0.15f,0.99f,0.99f},
+				{0.15f,0.99f,0.99f},
+				{0.3f,0.99f,0.99f},
+				{0.3f,0.99f,0.99f},
+				{0.5f,0.99f,0.99f},
+				{0.5f,0.99f,0.99f}
+		};
+		
+		for(int y = 70,paint = 0,i = 0; y < BRICK_Y_OFFSET+NBRICK_ROWS*BRICK_HEIGHT + (NBRICK_ROWS-1)*BRICK_SEP; y += BRICK_HEIGHT + BRICK_SEP, paint++, i++) {
+			for(int x = BRICK_SEP/2,j = 0; x < APPLICATION_WIDTH; x += BRICK_WIDTH + BRICK_SEP, j++) {
+				bricks[i][j] = new GRect(x,y,BRICK_WIDTH,BRICK_HEIGHT);
+				bricks[i][j].setFilled(true);
+				bricks[i][j].setColor(Color.getHSBColor(colors[paint][0], colors[paint][1], colors[paint][2]));
+				add(bricks[i][j]);
+			}
+		}
+		
+		return bricks;
+	}
+
+	// function sets up paddle and returns GRect of paddle
+	private GRect setPaddle() {
+		GRect padle = new GRect(getWidth()/2 - PADDLE_WIDTH/2, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
+		padle.setFilled(true);
+		add(padle);
+		return padle;
+	}
+	
+	// same but on ball
+	private GOval setBall() {
+		GOval ball = new GOval(getWidth()/2 - BALL_RADIUS*2, getHeight()/2, BALL_RADIUS*2, BALL_RADIUS*2);
+		ball.setFilled(true);
+		add(ball);
+		return ball;
+	}
+	
+	// function sets up walls
+	private void setFrame() {
+		GRect frame1 = new GRect(0,0,WIDTH,HEIGHT);
+		GRect frame2 = new GRect(1,1,WIDTH-2,HEIGHT-2);
+		GRect frame3 = new GRect(2,2,WIDTH-4,HEIGHT-4);
+		GRect frame4 = new GRect(3,3,WIDTH-6,HEIGHT-6);
+		frame1.setColor(Color.BLACK);
+		frame2.setColor(Color.WHITE);
+		frame3.setColor(Color.gray);
+		frame4.setColor(Color.BLACK);
+		add(frame1);
+		add(frame2);
+		add(frame3);
+		add(frame4);
+	}
+	
+	// function is infinity loop which updates positions of game objects
+	private void startGame(GRect [][] bricks, GRect paddle, GOval ball) {
+		int life = NTURNS;
+		double ballMovementDirections [] = {(Math.random()-0.5)*4, 3}; // movement on X and Y
+		GLine marks [] = new GLine[100];
+		int count = 0; // count of loops in while true loop, i use it in order to add marks of ball's movement
+		
+		while(true) {
+			leaveMark(marks, ballMovementDirections, count, ball);
+			
+			// changing ball's position
+			ball.setLocation(ball.getX() + ballMovementDirections[0], ball.getY() + ballMovementDirections[1]);
+			
+			// moves paddle
+			double padleX = moveDirection(paddle.getX()); 
+			if((paddle.getX() + PADDLE_WIDTH < WIDTH && padleX == 1) || (paddle.getX() > 0 && padleX == -1)) {
+				paddle.setLocation(paddle.getX() + padleX*3, paddle.getY());
+			}
+			
+			delay();
+	
+			ballMovementDirections = directionChanges(ballMovementDirections, paddle, ball, bricks);
+			
+			// if there is no bricks left
+			if(ballMovementDirections[1] == 0) {
+				victoryEmote();
+				break;
+			}
+			
+			// if ball is out player loses one of the lives
+			life = looseBall(ball, paddle, ballMovementDirections, life);
+			
+			if(life == 0) {
+				loserEmote();
+				break;
+			}
+			
+			count++;
+		}
+	}
+	
+	// function returns in which direction paddle should move
+	private double moveDirection(double padleX) {
+		// if mouse pointer is in the middle of paddle in order for it to not start shaking left and right
+		// i made it so pointer should be away from paddles middle by at least 2 pixels to make paddle move
+		if(MouseInfo.getPointerInfo().getLocation().getX()-(padleX + PADDLE_WIDTH - 5) > 2) {
+			return 1;
+		} else if (MouseInfo.getPointerInfo().getLocation().getX()-(padleX + PADDLE_WIDTH - 5) < -2){
+			return -1;
+		}
+		return 0;
+	}
+	
+	// function leaves lines so you know which movements ball did, it leaves up to 100 lines
+	private void leaveMark(GLine [] marks, double [] ballMovementDirections, int count, GOval ball) {
+		if(marks[count%100] != null) {
+			remove(marks[count%100]);
+		}
+		double r = BALL_RADIUS;
+		marks[count%100] = new GLine(ball.getX() + r, ball.getY() + r,
+				ball.getX() + ballMovementDirections[0] + r, ball.getY() + ballMovementDirections[1] + r);
+		add(marks[count%100]);
+	}
+	
+	// function makes program have little delay to make it playable, otherwise everything will happen too fast
+	private void delay() {
+		try {
+		    Thread.sleep(7); 
+		} catch (InterruptedException e) {
+		    Thread.currentThread().interrupt();
+		}
+	}
+
+	// function changes ball's directions according to where did it hit
+	private double [] directionChanges(double [] ballMovementDirections, GRect paddle, GOval ball, GRect [][] bricks) {
+		// when ball hits right wall
+		if(ball.getX() >= WIDTH - BALL_RADIUS*2) {
+			ballMovementDirections[0] = -ballMovementDirections[0];
+		}
+		// when ball hits left wall
+		if(ball.getX() <= 0) {
+			ballMovementDirections[0] = -ballMovementDirections[0];
+		}
+		// when ball hits top wall
+		if(ball.getY() <= 5) {
+			ballMovementDirections[1] = -ballMovementDirections[1];
+		}
+		// when ball hits paddle
+		if(ball.getX() + BALL_RADIUS*2 > paddle.getX() && ball.getX() < paddle.getX() + PADDLE_WIDTH && ball.getY() >= paddle.getY() - BALL_RADIUS*2) {
+			ballMovementDirections[1] = -ballMovementDirections[1];
+		}
+		// when ball hits blocks
+		boolean brickIsLeft = false;
+		boolean directionChanged = false;
+		for(int i = 0; i < NBRICK_ROWS; i++) {
+			for(int j = 0; j < NBRICKS_PER_ROW; j++) {
+				// when block is already taken out
+				if(bricks[i][j].isFilled() == false || directionChanged) {
+					continue;
+				}
+				brickIsLeft = true;
+				// when ball hits block from left or from right
+				if((ball.getY() + BALL_RADIUS <= bricks[i][j].getY() + BRICK_HEIGHT && ball.getY() + BALL_RADIUS >= bricks[i][j].getY()) &&
+						((ball.getX() + BALL_RADIUS*2 >= bricks[i][j].getX() && ball.getX() + BALL_RADIUS*2 <= bricks[i][j].getX() + ballMovementDirections[0] + 2) ||
+						(ball.getX() <= bricks[i][j].getX() + BRICK_WIDTH && ball.getX() >= bricks[i][j].getX() + BRICK_WIDTH + ballMovementDirections[0] - 2))) {
+					bricks[i][j].setFilled(false);
+					remove(bricks[i][j]);
+					ballMovementDirections[0] *= -1;
+					directionChanged = true;
+				} else {
+					// when ball hits block from top or from bottom
+					if(ball.getX() + BALL_RADIUS*2 >= bricks[i][j].getX() && ball.getX() <= bricks[i][j].getX() + BRICK_WIDTH &&
+							((ball.getY() >= bricks[i][j].getY() + BRICK_HEIGHT + ballMovementDirections[1] - 2 && ball.getY() <= bricks[i][j].getY() + BRICK_HEIGHT) ||
+							(ball.getY() + BALL_RADIUS*2 >= bricks[i][j].getY() && ball.getY() + BALL_RADIUS*2 <= bricks[i][j].getY()+ballMovementDirections[1]+2))) {
+						bricks[i][j].setFilled(false);
+						remove(bricks[i][j]);
+						ballMovementDirections[1] = -ballMovementDirections[1];
+						directionChanged = true;
+					}
+				}
+			}
+		}
+		// if there is not any bricks left this stops game
+		if(brickIsLeft == false) {
+			ballMovementDirections[1] = 0;
+		}
+		return ballMovementDirections;
+	}
+	
+	// function checks if player did not manage save the ball and in that case resets locations of paddle and ball
+	private int looseBall(GOval ball, GRect paddle, double [] ballMovementDirections, int life) {
+		if(ball.getY() > paddle.getY()) {
+			ball.setLocation(getWidth()/2 - BALL_RADIUS, getHeight()/2);
+			paddle.setLocation(getWidth()/2 - PADDLE_WIDTH/2, getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
+			ballMovementDirections[0] = (Math.random()-0.5)*4;
+			ballMovementDirections[1] = 3;
+			life--;
+		}
+		return life;
+	}
+
+	// function clears window and prints message
+	private void victoryEmote() {
+		removeAll();
+		println("Congratulations! You wasted time winning this pointless game!");
+	}
+
+	// same as victoryEmote
+	private void loserEmote() {
+		removeAll();
+		println("You play boring game like this one and did not even win?! Think about your life more.");
+	}
+}
+
+
