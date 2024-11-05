bc1131a2b083b7819aa801e3b38948e14bb730cb
S <s>
2024-11-05 16:53:22 +0400

    {"types":"C","runs":7}

--- advancedBreakout.java
+++ advancedBreakout.java
@@ -84,7 +84,7 @@ public class advancedBreakout extends GraphicsProgram {
 /** laser shot */
 	private boolean mouseIsDown = false;
 	private boolean laserIsAvaliable = true;
-	private double LASER_WIDTH = 16;
+	private double LASER_WIDTH = 46;
 /* Method: run() */
 
 	public void run() {
