f2fc61bb56fec1455fab4ee0212fa09efa7638e1
S <s>
2024-11-12 02:21:36 +0400

    {"types":"C","runs":25}

--- advancedBreakout.java
+++ advancedBreakout.java
@@ -73,7 +73,7 @@ public class advancedBreakout extends GraphicsProgram {
 	private static final double LASER_WIDTH = 8;
 	
 /** Radius of popping in bomb mode (after popping white block bomb mode activates */
-	private static final double ANNIHILATION_RADIUS = 77;
+	private static final double ANNIHILATION_RADIUS = 17;
 	
 ///////////////////// changeable global variables ////////////////////// 
 	
