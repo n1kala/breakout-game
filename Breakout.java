d3af5324626a81f3f0071e2aa3715d4257365ba9
S <s>
2024-11-12 20:50:36 +0400

    {"types":"C","runs":62}

--- Breakout.java
+++ Breakout.java
@@ -199,7 +199,6 @@ public class Breakout extends GraphicsProgram {
 				removeAll();
 				GLabel loseEmote = new GLabel("You lose.", WIDTH/2 - 20, HEIGHT/2 - 20);
 				loseEmote.setColor(Color.RED);
-				setFont(loseEmote.getFont() + 100);
 				add(loseEmote);
 				break;
 			}
